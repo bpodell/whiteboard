a3e163f56d5eb63a5d240ea0cb2240d891e98dca
billodell <billodell5@gmail.com>
2018-01-22 16:18:16 -0800

    tests passing

--- 06/README.md
+++ 06/README.md
@@ -1,1 +1,3 @@
 # whiteboard
+
+Write a recursive function called loop that has the function signature (count, callback) => undefined It should call the callback count times (count is expected to be > 0)
--- 06/lib/solution.js
+++ 06/lib/solution.js
@@ -1,13 +1,18 @@
-function loop(count, callback) {
+'use strict';
+
+module.exports = function loop(count, callback) {
+  if (count < 1 || isNaN(count)) {
+    return 'invalid count';
+  }
   if (count > 0) {
     callback();
     count --;
     loop(count, callback);
   }
-}
+};
 
-function test(){
-  console.log('hi');
-}
+// function test(){
+//   console.log('hi');
+// }
 
-loop(5,test);
+// loop(5,test);
