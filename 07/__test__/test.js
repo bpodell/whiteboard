35e9cc2a34bdc1d974fc215b9843d5f82ad496e2
billodell <billodell5@gmail.com>
2018-01-23 21:25:09 -0800

    submission

--- 07/__test__/test.js
+++ 07/__test__/test.js
@@ -12,7 +12,7 @@ describe('#Link', function () {
   it('should take in a circular list and return true', function () {
     expect(link(cll)).toEqual(true);
   });
-  it('should return error if input is invalid', function () {
+  it('should throw error if input is invalid', function () {
     expect(() => {
       link(123);
     }).toThrow();
