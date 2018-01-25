1de145a189ed9803bb0d58a60ea53d62f6cb802e
billodell <billodell5@gmail.com>
2018-01-24 21:52:13 -0800

    readme updated

--- 08/lib/solution.js
+++ 08/lib/solution.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const SLL = require('./link.js');
-const Node = require('./node.js');
+
 
 module.exports = function (sll1, sll2) {
-  console.log(typeof sll1);
   if(typeof sll1 !== 'object' || typeof sll2 !== 'object') return null;
   sll1 = sll1.head;
   sll2 = sll2.head;
@@ -22,16 +21,7 @@ module.exports = function (sll1, sll2) {
   }
   let answer = new SLL;
   arr2.forEach(i => answer.insertEnd(i));
-  console.log(answer);
   return answer;
-}
+};
 
 
-let test = new SLL;
-let test2 = new SLL;
-test.insertEnd(1);
-test.insertEnd(2);
-test.insertEnd(3);
-test2.insertEnd(1);
-test2.insertEnd(3);
-// intersect(test, test2);
