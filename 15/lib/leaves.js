d937a10374bdf7eb0fb8355670e6e71bc978ec5c
billodell <billodell5@gmail.com>
2018-02-02 14:51:17 -0800

    finished

---
+++ 15/lib/leaves.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Kary = require('./kary-tree');
+
+const leaf = module.exports = {};
+
+leaf.arr = [];
+
+leaf.leaves = (current) => {
+  if (current.val.children.length === 0) {
+    leaf.arr.push(current);
+  }
+};
+
+leaf.prune = (tree) => {
+  if(typeof tree !== 'object') throw new Error('whoops');
+  if(tree.root.children.length === 0) {
+    return [tree];
+  }
+  tree.breadthFirst(leaf.leaves);
+  return leaf.arr;
+};
+
